02ca853b3a51488d4ad91ae85134b0666098537e
nguyenngoctoan201005 <nguyenngoctoan201005@gmail.com>
2025-09-28 15:17:33 +0700

    test

---
+++ test.js
@@ -0,0 +1,12 @@
+[{
+   MaDonHang : 'DH001',
+   KhachHang : 'Nguyen Van A',
+   TrangThai : 'Da giao',
+   TongTien : 1500000,
+   NgayDat : '2023-10-01',
+   SanPham : [
+      { TenSP : 'Ao So Mi', SoLuong : 2, Gia : 300000 },
+      { TenSP : 'Quan Tay', SoLuong : 1, Gia : 600000 },
+      { TenSP : 'Giay Da', SoLuong : 1, Gia : 300000 }
+   ]
+}]
